a615ab76dae0ac105e29bcd9c6f9a3a667465d45
Anael <anaelmparaujo@gmail.com>
2025-11-03 10:47:51 -0300

    Delete CRUD directory

--- CRUD/tabela pessoas presenteadas (simplificado).sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE pessoas_presenteadas (
-	id_presenteado SERIAL PRIMARY KEY,
-	id_pedido INT REFERENCES pedidos_lencos(id_pedido),
-	nome_presenteado VARCHAR(150) NOT NULL
-);
-SELECT * FROM pessoas_presenteadas;
--- CRUD/tabela solicitantes (simplificado).sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE solicitantes (
-	id_solicitante SERIAL PRIMARY KEY,
-	nome_completo VARCHAR(150) NOT NULL,
-	email VARCHAR(50) UNIQUE NOT NULL,
-	telefone VARCHAR(20),
-	data_solicitacao DATE DEFAULT CURRENT_DATE
-);
-SELECT * FROM solicitantes;
